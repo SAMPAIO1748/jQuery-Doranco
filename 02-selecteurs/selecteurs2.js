964df94736aa4c17cab80a6ad0e269f4f1b4ddf0
SAMPAIO1748 <57705851+SAMPAIO1748@users.noreply.github.com>
2023-01-23 12:26:39 +0100

    selecteur exercice correction

--- 02-selecteurs/selecteurs2.js
+++ 02-selecteurs/selecteurs2.js
@@ -2,4 +2,12 @@ $(document).ready(function () {
   console.log("jquery est chargé");
 
   // Exercice selectionner l'élément qui a l'id maSection et remplacer le texte d'origin par "<h2>Bonjour à tous</h2>"
+
+  let maSection = $("#maSection");
+
+  console.log(maSection.html());
+
+  maSection.html("<h2>Bonjour à tous</h2>");
+  // la fonction html() de jQuery est équivalent à innerHTML() en JavaScript, elle permet de remplacer ou de d'ajouter
+  // du contenu html à l'élément sélectioné ainsi que de récupérer ce contenu pour l'afficher (par exemple).
 });
