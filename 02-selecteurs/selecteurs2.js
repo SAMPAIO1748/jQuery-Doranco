30356d532f2362eccdad3bbacaeff3a6dc8db67f
SAMPAIO1748 <57705851+SAMPAIO1748@users.noreply.github.com>
2023-01-23 12:07:44 +0100

    selecteur exercice

---
+++ 02-selecteurs/selecteurs2.js
@@ -0,0 +1,5 @@
+$(document).ready(function () {
+  console.log("jquery est chargé");
+
+  // Exercice selectionner l'élément qui a l'id maSection et remplacer le texte d'origin par "<h2>Bonjour à tous</h2>"
+});
